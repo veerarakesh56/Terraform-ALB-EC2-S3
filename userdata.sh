451d164bb30df87d7c232d28ed99b85b7310a261
Veera Rakesh Kethari <veerarakesh56@gmail.com>
2024-07-15 23:32:58 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -32,7 +32,7 @@ cat <<EOF > /var/www/html/index.html
 <body>
   <h1>Terraform Project Server 1</h1>
   <h2>Instance ID: <span style="color:green">$INSTANCE_ID</span></h2>
-  <p>Welcome to Veera Rakesh's Channel-1</p>
+  <p>Welcome to Veera Rakesh's Channel</p>
   
 </body>
 </html>
